165c35ca31e1534debb1cec736abf4883e048b4f
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-09-11 18:16:18 +0200

    Update bruteforce.cpp

--- 1. Two sum/bruteforce.cpp
+++ 1. Two sum/bruteforce.cpp
@@ -1,19 +1,16 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-
         for (int i = 0; i < nums.size() - 1; ++i) {
             for (int j = i + 1; j < nums.size(); ++j) {
                 if (nums[i] + nums[j] == target) {
-                    res.push_back(i);
-                    res.push_back(j);
-
-                    return res;
+                    return {i, j};
                 }
             }
         }
         
-        return res;
+        return {};
     }
 };
