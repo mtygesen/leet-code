b5466e9d577288339d8ad165bd107d4baf38afdf
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-09-12 14:20:40 +0200

    Update hashmap.cpp

--- 1-two_sum/hashmap.cpp
+++ 1-two_sum/hashmap.cpp
@@ -2,7 +2,7 @@
 // Space Complexity: O(n)
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(std::vector<int>& nums, int target) {
         std::unordered_map<int, int> mp;
 
         for (int i = 0; i < nums.size(); ++i) {
