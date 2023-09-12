3dba703f0726ebff335b2082938a0ad5dabb5e69
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-09-12 13:23:50 +0200

    Create solution.cpp

---
+++ 3-longest_substring_without_repeating_characters/solution.cpp
@@ -0,0 +1,25 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        
+        int max = 0;
+        int l = 0; 
+
+        for (int r = 0; r < s.length(); ++r) {
+            if (!mp.contains(s[r]) || mp[s[r]] < l) {
+                mp[s[r]] = r;
+                max = std::max(max, r - l + 1);
+            }
+            else {
+                l = mp[s[r]] + 1;
+                mp[s[r]] = r;
+            }
+        }
+
+
+        return max;
+    }
+};
